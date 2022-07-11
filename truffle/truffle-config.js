2c1e310d0da8948964d1db59df3e02bce2816e1c
root <root@DESKTOP-N2M4JQD.localdomain>
2022-07-11 17:22:45 +0200

    third commit

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -3,7 +3,7 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 require('dotenv').config();
 
 module.exports = {
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  contracts_build_directory: path.join(__dirname, "../client/src/contracts"),
   networks: { 
    development: {
     host: "127.0.0.1",     // Localhost (default: none)
